9041bf1bb86161099ccab6bdad6bf9041d65aab7
eduardodelreal <pablo.e.delreal@gmail.com>
2024-01-25 23:31:02 +0100

    cracion df de interest en df_order

--- src/module_2/module_2_data_prep.py
+++ src/module_2/module_2_data_prep.py
@@ -1,6 +1,10 @@
 import boto3
+import pandas as pd
 import os
 
+import datetime
+
+'''
 # Inicializar el cliente S3
 s3 = boto3.client('s3')
 
@@ -21,11 +25,79 @@ for obj in objects.get('Contents', []):
     if not key.endswith('/'):  # Ignorar carpetas (keys que terminan en '/')
         destination_path = os.path.join(local_directory, key.split('/')[-1])
         s3.download_file(bucket_name, key, destination_path)
+'''
+
+#first of all we do a little of analysis, as we open all the different datasets
+
+df_inventory = pd.read_parquet('local_data/inventory.parquet', engine='pyarrow')
+df_abandoned_carts = pd.read_parquet('local_data/abandoned_carts.parquet', engine='pyarrow')
+df_orders = pd.read_parquet('local_data/orders.parquet', engine='pyarrow')
+df_regulars = pd.read_parquet('local_data/regulars.parquet', engine='pyarrow')
+df_users = pd.read_parquet('local_data/users.parquet', engine='pyarrow')
+
+
+#funciones de prueba
+def mean_price():
+    price_column=df_inventory["price"]
+    mean_price = price_column.mean()
+    print(mean_price)
+
+def order_dates(n:int):
+    
+    dates = pd.to_datetime(df_orders['order_date'])
+    new_dates= dates.to_list()
+    if n==0: #si es cero que nos printee todo
+        print(new_dates)
+    else:
+        for i in range(n): #sino que nos de un rango de fechas especificas
+            print(new_dates[i])
+            
+def orders_lista():
+    # Primero, necesitas convertir esa columna en una serie de valores individuales.
+    # Puedes hacer esto con el método 'explode' de Pandas:
+    exploded_items = df_orders['ordered_items'].explode()
+
+    # Ahora, 'exploded_items' es una Serie donde cada 'item_id' está en su propia fila.
+    # Puedes hacer un recuento de la frecuencia de cada 'item_id' con value_counts():   
+    item_counts = exploded_items.value_counts()
+
+# 'item_counts' es ahora una Serie donde el índice es el 'item_id' y el valor es el recuento de frecuencias.
+    print(item_counts)
+
+# Convertimos la serie de recuentos en un DataFrame con reset_index
+    item_counts_df_oders = item_counts.reset_index()
+    item_counts_df_oders.columns = ['product_id', 'number_of_orders']
+
+# Calculamos el número total de orders
+    total_orders = len(df_orders)
+
+# Agregamos la columna de probabilidad de compra dividiendo por el número total de órdenes
+    item_counts_df_oders['purchase_probability'] = item_counts_df_oders['number_of_orders'] / total_orders
+
+# Mostramos los primeros elementos para verificar
+    print(item_counts_df_oders.head())
+    
+    
+    '''
+    lista=[]
+    
+    exploded_items = df_orders['ordered_items'].explode()
+
+    item_counts = exploded_items.value_counts()
+
+    print(item_counts)
+    '''
+
+            
 
 
 
 def main():
-    raise NotImplementedError
+   # mean_price()
+   # order_dates(5)
+   orders_lista()
+    
+
 
 if __name__ == "__main__":
     main()
