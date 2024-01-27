ccadaa23b2e685b2db28b6611cc491669a501e86
eduardodelreal <pablo.e.delreal@gmail.com>
2024-01-27 21:24:53 +0100

    .

--- src/module_2/module_2_data_prep.py
+++ src/module_2/module_2_data_prep.py
@@ -102,57 +102,6 @@ def test_combinar_datasets_totales():
     print("Todas las pruebas pasaron correctamente.")
     
 
-    
-
-
-#para el plot; focus on :
-#Top 10 productos por frecuencia de compra.
-#Top 10 productos por probabilidad de compra.
-#Top 10 productos por frecuencia de abandono.
-#Top 10 productos por probabilidad de abandono.
-
-'''
-def plot_combined_dataset():
-    
-    df_combined=combinar_datasets_totales()
-    
-    # Configuración de la visualización
-    plt.figure(figsize=(20, 10))
-
-    # Top 10 productos por frecuencia de compra
-    plt.subplot(2, 2, 1)
-    sns.barplot(x='product_id', y='number_of_orders', data=df_combined.sort_values('number_of_orders', ascending=False).head(10))
-    plt.title('Top 10 Productos por Frecuencia de Compra')
-    plt.xticks(rotation=45)
-
-    # Top 10 productos por probabilidad de compra
-    plt.subplot(2, 2, 2)
-    sns.barplot(x='product_id', y='purchase_probability', data=df_combined.sort_values('purchase_probability', ascending=False).head(10))
-    plt.title('Top 10 Productos por Probabilidad de Compra')
-    plt.xticks(rotation=45)
-
-    # Top 10 productos por frecuencia de abandono
-    plt.subplot(2, 2, 3)
-    sns.barplot(x='product_id', y='number_of_abandoned', data=df_combined.sort_values('number_of_abandoned', ascending=False).head(10))
-    plt.title('Top 10 Productos por Frecuencia de Abandono')
-    plt.xticks(rotation=45)
-
-    # Top 10 productos por probabilidad de abandono
-    plt.subplot(2, 2, 4)
-    sns.barplot(x='product_id', y='abandon_probability', data=df_combined.sort_values('abandon_probability', ascending=False).head(10))
-    plt.title('Top 10 Productos por Probabilidad de Abandono')
-    plt.xticks(rotation=45)
-
-    plt.tight_layout()
-    plt.show()
-
-'''
-
-    
-
-            
-
-
 
 def main():
    # mean_price()
@@ -162,7 +111,8 @@ def main():
    #print(len(df_orders))
    #combinar_datasets_prod_pedidos_inventario()
    combinar_datasets_totales()
-   test_combinar_datasets_totales()
+   #test_combinar_datasets_totales()
+   #test_plot_combined_dataset()
    #plot_combined_dataset()
    #hours_vs_orders_plot(df_abandoned_carts,'created_at')
    
